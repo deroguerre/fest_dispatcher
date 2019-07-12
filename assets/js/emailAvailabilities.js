89ad941e363d249c5566c404d57681a426577354
gui <deroguerre@gmail.com>
2019-07-12 20:05:11 +0200

    fix ckeditor

--- assets/js/emailAvailabilities.js
+++ assets/js/emailAvailabilities.js
@@ -5,30 +5,38 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 require("startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min");
 
 $(document).ready(function () {
+
+    //init ckeditor
+    ClassicEditor
+        .create(document.querySelector('#prepare_email_availabilities_body'), {
+
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    //init dataTable
     $('#volunteers_table').DataTable();
-});
 
-ClassicEditor
-    .create(document.querySelector('#prepare_email_availibilities_body'))
-    .catch(error => {
-        console.error(error);
+    var ignoredUsers = [];
+
+    //prevent unchecked box after refresh
+    $(".volunteers-checkbox").prop("checked", true);
+
+    //click on users checkbox
+    $(".volunteers-checkbox").click(function () {
+        if ($(this).is(':checked')) {
+            remove(ignoredUsers, $(this).val());
+            $('[name=ignoredUsers]').val(ignoredUsers);
+        } else {
+            ignoredUsers.push($(this).val());
+            $('[name=ignoredUsers]').val(ignoredUsers);
+        }
     });
-
-var ignoredUsers = [];
-
-//prevent unchecked box after refresh
-$(".volunteers-checkbox").prop( "checked", true );
-
-$(".volunteers-checkbox").click(function () {
-    if ($(this).is(':checked')) {
-        remove(ignoredUsers, $(this).val());
-        $('[name=volunteers]').val(ignoredUsers);
-    } else {
-        ignoredUsers.push($(this).val());
-        $('[name=volunteers]').val(ignoredUsers);
-    }
 });
 
+
+//remove value into array
 function remove(array, element) {
     const index = array.indexOf(element);
     array.splice(index, 1);
