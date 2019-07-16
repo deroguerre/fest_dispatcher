a94bdbc646714269edf65a25bf055cc6282db211
spelissier <pelissiersebastien@gmail.com>
2019-07-16 13:03:08 +0200

    form Volunteer availability is ok

---
+++ src/Form/VolunteerAvailabilityForUserType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\VolunteerAvailability;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VolunteerAvailabilityForUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startDate')
+            ->add('endDate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => VolunteerAvailability::class,
+        ]);
+    }
+}
