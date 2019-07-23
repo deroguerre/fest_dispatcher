328a5b613140d67c9036af8d1be13be29f68eb29
spelissier <pelissiersebastien@gmail.com>
2019-07-23 10:24:47 +0200

    event listener

--- src/Service/AnswerAvailibilities/EventListener/AnswerSubscriber.php
+++ src/Service/AnswerAvailibilities/EventListener/AnswerSubscriber.php
@@ -3,51 +3,40 @@
 
 namespace App\Service\AnswerAvailibilities\EventListener;
 
-
 use App\Entity\VolunteerAvailability;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Workflow\Event\Event;
 
-
-/**
- * @property LoggerInterface logger
- */
 class AnswerSubscriber implements EventSubscriberInterface
 {
     private $logger;
-
     public function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
 
     /**
-     * @return array The event names to listen to
+     * @return array
      */
     public static function getSubscribedEvents()
     {
         return [
-            'workflow.volunteer_availibilities_workflow.completed.draft_email' => 'onNewAnswer'
+            'workflow.volunteer_availibilities_workflow.completed.volunteer_answer' => 'onNewAnswer'
         ];
     }
 
     /**
-     * created email
      * @param Event $event
      */
     public function onNewAnswer(Event $event)
-    {
-        /**
-         * @var VolunteerAvailability $emailalerte
-         */
-        $emailalerte = $event->getSubject();
+{
 
-        #save in log
-        $this->logger->info('nouvel email pour le festival' . $emailalerte->getFestival()->getName());
-    }
-    #notication au manager par email
 
+    /** @var  VolunteerAvailability $volunteerAvailability */
+    $volunteerAvailability = $event->getSubject();
+    $this->logger->info('Le bénévole'. $volunteerAvailability->getUser() .'a ajouté des disponibilités ');
+}
 
 
 
